904a192ffc9810d380d3acecc4608548e208718c
devgui <guihermepereiradv@gmail.com>
2024-12-07 10:48:52 -0300

    feat(users): add model atributtes

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,35 @@
-import { Table } from 'sequelize-typescript';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
-@Table
-export class User {}
+@Table({
+  tableName: 'users',
+  timestamps: false,
+})
+export class User extends Model {
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    primaryKey: true,
+    defaultValue: DataType.UUIDV4,
+  })
+  id: string;
+  @Column({
+    type: DataType.STRING(30),
+    allowNull: false,
+  })
+  username: string;
+  @Column({
+    type: DataType.STRING(60),
+    allowNull: false,
+  })
+  password: string;
+  @Column({
+    type: DataType.TIME,
+    defaultValue: DataType.NOW(),
+  })
+  created_at: Date;
+  @Column({
+    type: DataType.TIME,
+    defaultValue: DataType.NOW(),
+  })
+  updated_at: Date;
+}

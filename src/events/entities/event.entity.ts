c0c9d1aee0dfc2886f80616a6eead29baaa68715
devgui <guihermepereiradv@gmail.com>
2024-12-09 17:39:37 -0300

    feat(event.model): adjust event.entity

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,1 +1,35 @@
-export class Event {}
+import { Column, DataType, HasMany, Model, Table } from 'sequelize-typescript';
+import { Presence } from 'src/presences/entities/presence.entity';
+
+@Table({
+  tableName: 'events',
+  timestamps: false,
+})
+export class Event extends Model {
+  @Column({
+    type: DataType.STRING,
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  id: number;
+  @Column({
+    type: DataType.STRING(60),
+    allowNull: false,
+  })
+  name: string;
+  @Column({
+    type: DataType.DATE,
+    allowNull: false,
+    defaultValue: DataType.NOW,
+  })
+  created_at: Date;
+  @Column({
+    type: DataType.DATE,
+    allowNull: false,
+    defaultValue: DataType.NOW,
+  })
+  updated_at: Date;
+
+  @HasMany(() => Presence)
+  presences: Presence[];
+}

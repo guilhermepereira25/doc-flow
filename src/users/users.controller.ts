f87a7b7d8b48d912f139655302e757ef844bd73b
devgui <guihermepereiradv@gmail.com>
2024-12-11 23:13:20 -0300

    feat: adjust user controller to support new changes and relation with profile

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,18 +6,37 @@ import {
   Patch,
   Param,
   Delete,
+  Res,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Response } from 'express';
+import { Profiles } from 'src/profile/decorators/profile.decorator';
+import { Profile } from 'src/profile/enum/profile.enum';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Profiles(Profile.Admin, Profile.Professor)
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Res() res: Response, @Body() createUserDto: CreateUserDto) {
+    try {
+      const user = await this.usersService.create(createUserDto);
+      if (!user) {
+        return res.status(409).json({ message: 'User already exists' });
+      }
+      return res.status(201).json(user);
+    } catch (err) {
+      if (process.env.APP_ENV === 'development') {
+        console.error(err);
+      }
+      if (err instanceof Error && err.message === 'Profile not found') {
+        return res.status(400).json({ message: 'Invalid profile' });
+      }
+      return res.status(500).json({ message: 'Internal server error' });
+    }
   }
 
   @Get('page/:page')
@@ -30,13 +49,30 @@ export class UsersController {
     return this.usersService.findOne(id);
   }
 
+  @Profiles(Profile.Admin, Profile.Professor)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(id, updateUserDto);
+  async update(
+    @Res() res: Response,
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    try {
+      const user = await this.usersService.update(id, updateUserDto);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      return res.status(200).json(user);
+    } catch (err) {
+      if (process.env.APP_ENV === 'development') {
+        console.error(err);
+      }
+      return res.status(500).json({ message: 'Internal server error' });
+    }
   }
 
+  @Profiles(Profile.Admin, Profile.Professor)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(id);
+  async remove(@Param('id') id: string) {
+    return await this.usersService.remove(id);
   }
 }

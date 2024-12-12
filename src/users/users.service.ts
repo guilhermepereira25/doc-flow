2e2aeb20e2ee86b7625886af5556d50c66d423a1
devgui <guihermepereiradv@gmail.com>
2024-12-11 23:13:01 -0300

    feat: define a defaultProfile user when create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,16 +3,32 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserRepository } from './repositories/user.repository.interface';
 import { USER_REPOSITORY } from './repositories/user-repository.token';
-
+import { ProfileService } from '../profile/profile.service';
+import { Profile as ProfileEnum } from 'src/profile/enum/profile.enum';
+import { Profile as ProfileModel } from 'src/profile/entities/profile.entity';
 @Injectable()
 export class UsersService {
   constructor(
     @Inject(USER_REPOSITORY)
     private userRepository: UserRepository,
+    private readonly profileService: ProfileService,
   ) {}
 
   async create(createUserDto: CreateUserDto) {
-    return await this.userRepository.create(createUserDto);
+    let profile: ProfileModel | null;
+    if (!createUserDto.profileId) {
+      profile = await this.profileService.findByProfileName(ProfileEnum.User);
+      if (!profile) {
+        throw new Error('Profile not found');
+      }
+      createUserDto.profileId = profile.id;
+    } else {
+      profile = await this.profileService.findOne(createUserDto.profileId);
+      if (!profile) {
+        throw new Error('Profile not found');
+      }
+    }
+    return await this.userRepository.create(createUserDto, profile.id);
   }
 
   async findAll(page: number) {

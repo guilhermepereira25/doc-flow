120648c017d07ba920bac94347805cfa85b58775
devgui <guihermepereiradv@gmail.com>
2024-12-14 14:18:15 -0300

    feat(events): add some validations when create a event

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,8 +1,14 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { EventRepository } from './repositories/event.repository.interface';
 import { Event } from './entities/event.entity';
+import { EventStatus } from './enum/event-status.enum';
 
 @Injectable()
 export class EventsService {
@@ -12,25 +18,56 @@ export class EventsService {
   ) {}
 
   async create(createEventDto: CreateEventDto): Promise<null | Event> {
-    const [event, created] = await this.eventRepository.findOrCreateEvent(
+    const event = await this.eventRepository.findEventByName(
       createEventDto.name,
     );
-    if (created) {
-      return null;
+    if (event) {
+      throw new ConflictException('Event already exists');
     }
-    return event;
+
+    const eventEndDate =
+      createEventDto.eventEndDate !== undefined
+        ? new Date(createEventDto.eventEndDate)
+        : null;
+    const now = new Date();
+    if (eventEndDate !== null && !this.isValidEventEndDate(eventEndDate, now)) {
+      throw new BadRequestException('Invalid event end date');
+    }
+
+    const eventStartDate = new Date(createEventDto.eventStartDate);
+    if (createEventDto.status !== undefined) {
+      const [result, message] = this.isValidEventStatusForEventDates(
+        eventStartDate,
+        eventEndDate,
+        now,
+        createEventDto.status,
+      );
+      if (!result && message !== null) {
+        throw new BadRequestException(message);
+      }
+    }
+
+    return await this.eventRepository.create({
+      name: createEventDto.name,
+      eventStartDate: eventStartDate.toISOString(),
+      eventEndDate: eventEndDate.toISOString(),
+      status: createEventDto.status,
+    });
   }
 
   async findAll(): Promise<Event[]> {
     return await this.eventRepository.findAll();
   }
 
-  async findOne(id: string): Promise<Event | null> {
+  async findOne(id: string): Promise<[Event, boolean, boolean] | null> {
     const event = await this.eventRepository.findOne(id);
     if (!event) {
       return null;
     }
-    return event;
+    const now = new Date();
+    const isStarted: boolean = this.eventStarted(event.start_at, now);
+    const isEnded: boolean = this.eventEnded(event.end_at, now);
+    return [event, isStarted, isEnded];
   }
 
   update(id: string, updateEventDto: UpdateEventDto) {
@@ -39,5 +76,62 @@ export class EventsService {
 
   async remove(id: string): Promise<void> {
     return await this.eventRepository.remove(id);
+  }
+
+  async endEvent(id: string): Promise<Event> {
+    return await this.eventRepository.endEvent(id);
+  }
+
+  async getStartedEvents(): Promise<Event[]> {
+    return await this.eventRepository.getStartedEvents();
+  }
+
+  async startEvent(id: string): Promise<Event> {
+    return await this.eventRepository.update(id, {
+      status: EventStatus.STATUS_STARTED,
+    });
+  }
+
+  private isValidEventEndDate(eventEndDate: Date, now: Date): boolean {
+    const datePlusThenMinutes = new Date(now.getMinutes() + 10);
+    if (eventEndDate < datePlusThenMinutes) {
+      return false;
+    }
+    if (eventEndDate < now) {
+      return false;
+    }
+    return true;
+  }
+
+  private isValidEventStatusForEventDates(
+    eventStartDate: Date,
+    eventEndDate: Date,
+    now: Date,
+    status: EventStatus,
+  ): [boolean, string | null] {
+    if (eventStartDate < now && status !== EventStatus.STATUS_STARTED) {
+      return [false, 'Event start date is in the past, status must be started'];
+    }
+    if (eventStartDate > now && status !== EventStatus.STATUS_UPCOMING) {
+      return [
+        false,
+        'Event start date is in the future, status must be upcoming',
+      ];
+    }
+    if (eventStartDate < now && eventEndDate < now) {
+      return [
+        false,
+        'Event start date and end date are in the past, status must be',
+      ];
+    }
+    return [true, null];
+  }
+
+  private eventStarted(start_at: Date, now: Date): boolean {
+    return start_at < now;
+  }
+
+  private eventEnded(end_at: Date, now: Date): boolean {
+    return end_at < now;
   }
 }

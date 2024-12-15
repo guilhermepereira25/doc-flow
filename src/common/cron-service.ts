d7ddf57ef18377c733a313995edad7a75934c9bc
devgui <guihermepereiradv@gmail.com>
2024-12-15 11:34:21 -0300

    feat: add abstract cronService class and change events-cron service

---
+++ src/common/cron-service.ts
@@ -0,0 +1,46 @@
+import { FileHandle, writeFile, open } from 'node:fs/promises';
+import * as path from 'path';
+
+export abstract class CronService {
+  protected logFile: FileHandle;
+
+  constructor(protected cronName: string) {
+    this.cronName = cronName;
+  }
+
+  private getFileName(cronJobName: string): string {
+    return (
+      new Date().toISOString().replace(/:/g, '-') +
+      '-' +
+      this.cronName +
+      cronJobName +
+      '.log'
+    );
+  }
+
+  protected async initLogFile(cronJobName: string): Promise<void> {
+    const fileName = this.getFileName(cronJobName);
+    this.logFile = await open(
+      path.resolve(__dirname, '../../', 'log/cron/', fileName),
+      'a',
+    );
+  }
+
+  protected async writeLog(message: string): Promise<void> {
+    if (!this.logFile) {
+      throw new Error('Log file not initialized');
+    }
+    await writeFile(
+      this.logFile,
+      `${new Date().toISOString()} - ${message}\n`,
+      'utf-8',
+    );
+  }
+
+  protected async closeLogFile(): Promise<void> {
+    if (!this.logFile) {
+      throw new Error('Log file not initialized');
+    }
+    await this.logFile.close();
+  }
+}
--- src/events/events-cron.service.ts
+++ src/events/events-cron.service.ts
@@ -1,96 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { EventsService } from './events.service';
-import { FileHandle, open, writeFile } from 'node:fs/promises';
-import * as path from 'path';
+import { CronService } from 'src/common/cron-service';
 
 @Injectable()
-export class EventCronService {
-  constructor(private readonly eventService: EventsService) {}
-
-  //TODO: implementar class abstract para reutilizar o código de inicialização do arquivo de log
-  private async initLogFile(cronName: string): Promise<FileHandle> {
-    const fileName =
-      new Date().toISOString().replace(/:/g, '-') + '-' + cronName + '.log';
-    const logFile: FileHandle = await open(
-      path.resolve(__dirname, '../../', 'log/cron/', fileName),
-      'a',
-    );
-    return logFile;
-  }
-
-  private async writeLog(logFile: FileHandle, message: string): Promise<void> {
-    await writeFile(
-      logFile,
-      `${new Date().toISOString()} - ${message}\n`,
-      'utf-8',
-    );
+export class EventCronService extends CronService {
+  constructor(private readonly eventService: EventsService) {
+    super('EventCronService');
   }
 
   // Executa a cada 10 minutos
   @Cron('0 */10 * * * *')
   async handleUpcomingToCheckIfEventsHasStarted() {
-    const logFile: FileHandle = await this.initLogFile(
-      'handleUpcomingToCheckIfEventsHasStarted',
-    );
+    await this.initLogFile('handleUpcomingToCheckIfEventsHasStarted');
 
-    await this.writeLog(
-      logFile,
-      'Iniciando a verificação de eventos pendentes.',
-    );
+    await this.writeLog('Iniciando a verificação de eventos pendentes.');
 
     try {
-      //TODO: not load all events in memory, try to use a cursor
       const pendingEvents = await this.eventService.getStartedEvents();
       if (pendingEvents.length === 0) {
-        await this.writeLog(logFile, 'Nenhum evento pendente.');
+        await this.writeLog('Nenhum evento pendente.');
         return;
       }
       for (const event of pendingEvents) {
         await this.eventService.startEvent(event.id);
-        await this.writeLog(
-          logFile,
-          `Evento ${event.name} iniciado com sucesso.`,
-        );
+        await this.writeLog(`Evento ${event.name} iniciado com sucesso.`);
       }
     } catch (err) {
-      await this.writeLog(logFile, `Erro ao iniciar eventos: ${err.message}`);
+      await this.writeLog(`Erro ao iniciar eventos: ${err.message}`);
     }
 
-    await this.writeLog(
-      logFile,
-      'Verificação de eventos pendentes finalizada.',
-    );
+    await this.writeLog('Verificação de eventos pendentes finalizada.');
 
-    logFile.close();
+    await this.closeLogFile();
   }
 
+  @Cron('0 */10 * * * *')
   async handleStartedToCheckIfEventsHasEnded() {
-    const logFile: FileHandle = await this.initLogFile(
-      'handleStartedToCheckIfEventsHasEnded',
-    );
+    await this.initLogFile('handleStartedToCheckIfEventsHasEnded');
 
-    await this.writeLog(
-      logFile,
-      'Iniciando a verificação de eventos iniciados.',
-    );
+    await this.writeLog('Iniciando a verificação de eventos iniciados.');
 
     try {
-      //TODO: not load all events in memory, try to use a cursor
       const startedEvents = await this.eventService.getStartedEvents();
       if (startedEvents.length === 0) {
-        await this.writeLog(logFile, 'Nenhum evento iniciado.');
+        await this.writeLog('Nenhum evento iniciado.');
         return;
       }
       for (const event of startedEvents) {
         await this.eventService.endEvent(event.id);
-        await this.writeLog(
-          logFile,
-          `Evento ${event.name} finalizado com sucesso.`,
-        );
+        await this.writeLog(`Evento ${event.name} finalizado com sucesso.`);
       }
     } catch (err) {
-      await this.writeLog(logFile, `Erro ao finalizar eventos: ${err.message}`);
+      await this.writeLog(`Erro ao finalizar eventos: ${err.message}`);
     }
   }
 }

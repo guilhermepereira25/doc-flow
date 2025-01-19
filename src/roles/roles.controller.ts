9d29e63828ad918d74fd5c310908ebcc6cc2dda5
devgui <guihermepereiradv@gmail.com>
2025-01-19 11:34:33 -0300

    refactor(src/roles): implements response dto

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   Res,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
@@ -16,6 +17,10 @@ import { Profiles } from 'src/profile/decorators/profile.decorator';
 import { Profile } from 'src/profile/enum/profile.enum';
 import { Roles } from './decorators/roles.decorator';
 import { Role } from './enum/role.enum';
+import { Role as RoleModel } from './entities/role.entity';
+import { GetAllRolesResponseDto } from './dto/get-all-roles-response.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiResponseDto } from 'src/lib/dto/api-response.dto';
 
 @Controller('roles')
 export class RolesController {
@@ -27,32 +32,122 @@ export class RolesController {
   async create(@Res() res: Response, @Body() createRoleDto: CreateRoleDto) {
     try {
       const role = await this.rolesService.create(createRoleDto);
-      res.status(201).json(role);
+      return res
+        .status(201)
+        .json(
+          new ApiResponseDto<{ role: RoleModel }>(201, true, { role }, null),
+        );
     } catch (err) {
       if (process.env.APP_ENV === 'development') {
         console.error(err);
       }
-      res.status(500).json({ message: err.message });
+      throw new InternalServerErrorException(
+        new ApiResponseDto<null>(500, false, null, 'Internal server error'),
+      );
     }
   }
 
+  @ApiOperation({ summary: 'Return all roles' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return all roles',
+    type: GetAllRolesResponseDto,
+  })
   @Get()
-  async findAll() {
-    return await this.rolesService.findAll();
+  async findAll(@Res() res: Response) {
+    try {
+      const rolesData = await this.rolesService.findAll();
+      return res
+        .status(200)
+        .json(
+          new ApiResponseDto<{ roles: RoleModel[] }>(
+            200,
+            true,
+            { roles: rolesData },
+            null,
+          ),
+        );
+    } catch (err) {
+      if (process.env.APP_ENV == 'development') {
+        console.error(err);
+      }
+      throw new InternalServerErrorException(
+        new ApiResponseDto<null>(500, false, null, 'Internal server error'),
+      );
+    }
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.rolesService.findOne(id);
+  async findOne(@Res() res: Response, @Param('id') id: string) {
+    try {
+      const role = await this.rolesService.findOne(id);
+      if (!role) {
+        return res
+          .status(404)
+          .json(new ApiResponseDto<null>(404, false, null, 'Role not found'));
+      }
+      return res
+        .status(200)
+        .json(
+          new ApiResponseDto<{ role: RoleModel }>(200, true, { role }, null),
+        );
+    } catch (err) {
+      if (process.env.APP_ENV === 'development') {
+        console.error(err);
+      }
+      throw new InternalServerErrorException(
+        new ApiResponseDto<null>(500, false, null, 'Internal server error'),
+      );
+    }
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return await this.rolesService.update(id, updateRoleDto);
+  async update(
+    @Res() res: Response,
+    @Param('id') id: string,
+    @Body() updateRoleDto: UpdateRoleDto,
+  ) {
+    try {
+      const updatedRole = await this.rolesService.update(id, updateRoleDto);
+      if (!updatedRole) {
+        return res
+          .status(404)
+          .json(new ApiResponseDto<null>(404, false, null, 'Role not found'));
+      }
+      return res
+        .status(200)
+        .json(
+          new ApiResponseDto<{ role: string }>(
+            200,
+            true,
+            { role: updatedRole },
+            null,
+          ),
+        );
+    } catch (err) {
+      if (process.env.APP_ENV === 'development') {
+        console.error(err);
+      }
+      throw new InternalServerErrorException(
+        new ApiResponseDto<null>(500, false, null, 'Internal server error'),
+      );
+    }
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
-    return await this.rolesService.remove(id);
+  async remove(@Res() res: Response, @Param('id') id: string) {
+    try {
+      await this.rolesService.remove(id);
+      return res
+        .status(200)
+        .json(new ApiResponseDto<object>(200, true, {}, null));
+    } catch (err) {
+      if (process.env.APP_ENV === 'development') {
+        console.error(err);
+      }
+      throw new InternalServerErrorException(
+        new ApiResponseDto<null>(500, false, null, 'Internal server error'),
+      );
+    }
   }
 }
